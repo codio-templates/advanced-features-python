6f9e1baf457192b352f9357db911915b7d299318
maximka <maxim.kraev@gmail.com>
2020-10-21 14:51:06 +0000

    Use `secure` location for unit tests
    * swtched `circle.py` to use `student_code` folder
    * moved the test case to `secure` folder
    * added import of student code using `path.append`

--- .guides/circle.py
+++
@@ -1,8 +0,0 @@
-from math import pi
-
-def circle_area(radius):
-#   if type(radius) not in [int, float]:
-#     raise TypeError("Radius must be a non-negative, real number.")
-#   if radius < 0:
-#     raise ValueError("Radius cannot be a negative number")
-  return pi * radius ** 2
